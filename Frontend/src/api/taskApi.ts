04080b5bbf893036c0562ecd883b310379dd8d1e
harsha-050 <harshamajji157@gmail.com>
2025-10-25 23:10:16 +0530

    feat: connect frontend to production backend on Render

--- Frontend/src/api/taskApi.ts
+++ Frontend/src/api/taskApi.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:5000/api'
+const BASE_URL = 'https://task-manager-api-v8fn.onrender.com/api'
 
 export interface TaskItem {
   id: number
